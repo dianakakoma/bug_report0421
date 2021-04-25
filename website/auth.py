62890f5b63b1a0cb5d4cc237abd1c874e5dd722a
dianakakoma <dikakoma@gmail.com>
2021-04-24 20:11:35 -0500

    created login, logout and sign_up routes for the users

--- website/auth.py
+++ website/auth.py
@@ -2,13 +2,14 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+#website routes/pages
 @auth.route('/login')
 def login():
     return "<p>Login</p>"
 
 @auth.route('/logout')
 def logout():
-    return "<p>Sign Up </p>"
+    return "<p>You are logged out. </p>"
 
 @auth.route('/sign-up')
 def signUp():
