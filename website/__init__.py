ba8b68e0bd71c90c04e0ea97b4ced002db43e5c1
dianakakoma <dikakoma@gmail.com>
2021-05-03 23:15:55 -0500

    wrote sign-up functionality that does not allow users with duplicate email addresses. Wrote login functionality, first verifies that email address exists in the system and then compares the hash of the pasword entered in the form, to the one already stored from the user.

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 #define and initialize the database
 db = SQLAlchemy()
@@ -8,11 +9,22 @@ DB_NAME = 'database.db'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret key'
-    app.config['SQLALCHEMY_DATABASE_URI'] - f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Report
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    #if the database does already exist them create one.
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+#import user security functionality - runs a hashing function on the password
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 #website routes/pages
 @auth.route('/login', methods=['GET','POST'])
 def login():
-    #get data entered in the login form
+    if request.method == 'POST':
+        #get data entered in the login form
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        #return the first result with a matching email address
+        user = User.query.filter_by(email=email).first()
+        if user:
+            #if the user is found, check the hashed password entered in the form against what is already hashed and stored
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash("Email does not exist.", category='error')
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -21,9 +39,14 @@ def signUp():
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-    
+
+        #search for existing email addresses to make sure that the email address does not already exist.
+        user= User.query.filter_by(email=email).first()
+
         # validating sign-up information & error message flashing
-        if len(email) < 4:
+        if user:
+            flash("Email already exists.", category='error')
+        elif len(email) < 4:
             flash("Email must be greater than 3 characters.", category='error')
         elif len(firstName) < 2:
             flash("First name must be greater than 1 character.", category='error')
@@ -31,7 +54,14 @@ def signUp():
             flash("Password length must be at least 7 characters.", category='error')
         elif password1 != password2:
             flash("Passwords must match.", category='error')
-        #if the inform
         else:
+            # if no errors have been encountered - create the new based on the database model
+            newUser = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(newUser)
+            db.session.commit()
+            
             flash('Account created!', category="success")
+
+            #after account is successfully created redirect the new user to the homepage.
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html")
